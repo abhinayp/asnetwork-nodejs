72b2f303c1573d831399adeab00a9515afd34115
Abhinay Parvathaneni <pabhinay43@gmail.com>
2018-08-30 12:58:13 +0530

    fix mongodb error by downgrading the version

--- routes/index.js
+++ routes/index.js
@@ -7,30 +7,30 @@ router.get('/', function(req, res, next) {
   var MongoClient = mongodb.MongoClient;
   var url = 'mongodb://localhost:27017/asnetwork';
 
-  // MongoClient.connect(url, function(err, db) {
-  //   if (err) {
-  //     console.log('Unable to connect')
-  //   }
-  //   else {
-  //     console.log('Connection establised');
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      console.log('Unable to connect')
+    }
+    else {
+      console.log('Connection establised');
 
-  //     var posts = db.collection('posts');
+      var posts = db.collection('posts');
 
-  //     posts.find({}).toArray(function(err, result) {
-  //       if (err) {
-  //         res.send(err);
-  //       }
-  //       else {
-  //         console.log(result);
-  //         res.render('index', {posts: result})
-  //       }
-  //     });
+      posts.find({}).toArray(function(err, result) {
+        if (err) {
+          res.send(err);
+        }
+        else {
+          console.log(result);
+          res.render('index', {posts: result})
+        }
+      });
 
-  //     db.close();
-  //   }
-  // });
+      db.close();
+    }
+  });
 
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
