cac5707a687cd1c49e480a5f0a5abe7fd686fdee
Abhinay Parvathaneni <pabhinay43@gmail.com>
2018-08-30 13:58:20 +0530

    Create Models and setup the structure of it

---
+++ models/posts.js
@@ -0,0 +1,62 @@
+var base = require('./../models/base');
+
+var posts = (function() {
+
+  var getAll = function(callback) {
+    base.mongoClient(function(db, status) {
+
+      if (status != 200) {
+        callback(null, 500, err);
+        return;
+      }
+
+      var posts = db.collection('posts');
+      posts.find({}).toArray(function(err, result) {
+        if (err) {
+          console.log(err);
+          callback(null, 500, err);
+        }
+        else {
+          console.log(result);
+          callback(result, 200);
+        }
+      });
+    })
+  };
+
+  var createPost = function(user, status, callback) {
+    base.mongoClient(function(db) {
+
+      if (status != 200) {
+        callback(null, 500, err);
+        return;
+      }
+
+      var d = new Date();
+      let posts = db.collection('posts');
+      let post = {
+        status: status,
+        created_at: d,
+        updated_at: d,
+        user: user.email,
+      }
+      posts.insert(post, function(err, result) {
+        if (err) {
+          console.log(err);
+          callback(null, 500, err);
+        }
+        else {
+          console.log(result);
+          callback(result);
+        }
+      });
+    });
+  };
+
+  return {
+    getAll: getAll,
+    createPost: createPost,
+  }
+})();
+
+module.exports = posts;
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,17 @@
 var express = require('express');
 var router = express.Router();
-var mongodb = require('mongodb');
+var posts = require('./../models/posts');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var MongoClient = mongodb.MongoClient;
-  var url = 'mongodb://localhost:27017/asnetwork';
 
-  MongoClient.connect(url, function(err, db) {
-    if (err) {
-      console.log('Unable to connect')
-    }
-    else {
-      console.log('Connection establised');
-
-      var posts = db.collection('posts');
-
-      posts.find({}).toArray(function(err, result) {
-        if (err) {
-          res.send(err);
-        }
-        else {
-          console.log(result);
-          res.render('index', {posts: result})
-        }
-      });
-
-      db.close();
-    }
+  posts.getAll(function(result) {
+    if (Array.isArray(result))
+      res.render('index', {posts: result});
+    else
+      res.render('index', {posts: []});
   });
 
-  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
